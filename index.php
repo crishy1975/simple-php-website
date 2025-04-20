ec2de3032961ad8ec9ca6b729cdb05a9164b0cd7
crishy <resch.ohg@gmail.com>
2025-04-20 08:58:55 +0200

    1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 1);
 require 'includes/config.php';
 require 'includes/functions.php';
 
-for (let n = 1; n <= 5; n++) {
+for (let n = 1; n <= 15; n++) {
     console.log("hoi");
 }
 
