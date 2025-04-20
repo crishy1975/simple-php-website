de2bd7f00756abc28bda7ea948dc50bfe288c03a
crishy <resch.ohg@gmail.com>
2025-04-20 08:56:42 +0200

    9

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 
 // Comment these lines to hide errors on dev gugu
 // branch dev like develoment
+// ich bin eine neue code zeile
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require 'includes/config.php';
 require 'includes/functions.php';
+
+for (let n = 1; n <= 5; n++) {
+    console.log("hoi");
+}
 
 init();
